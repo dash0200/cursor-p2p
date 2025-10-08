dcbcf1e89a3b72ab7fe68e1d1125d7bcdb7eae12
dash0200 <darshanvanakudre@gmail.com>
2025-10-09 01:03:48 +0530

    fixed error src/App.jsx from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import WebRTCManualSignal from './components/WebRTCManualSignal.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <WebRTCManualSignal />
   </StrictMode>,
 )
